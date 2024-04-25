2ebd77be834a891bd8a6ab427ddb806212ec70e1
terencepatrck <terence2.ramirez@live.uwe.ac.uk>
2024-04-26 00:06:54 +0100

    wew

--- Assets/DisplayInfo.cs
+++ Assets/DisplayInfo.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DisplayInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject infoPanel; // Assign this in the inspector to your UI Panel
+    public TextMeshProUGUI infoText; // Assign your TextMeshProUGUI component
+    public Image infoImage; // Assign your Image component if you want to change the image dynamically
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // Make sure your player GameObject is tagged as "Player"
+        {
+            ShowInfoPanel();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowInfoPanel()
     {
-        
+        infoPanel.SetActive(true); // Show the info panel
+
+        // Unlock and show the cursor when displaying the UI
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        // Optional: Set text and image properties here if they need to change dynamically
+        // infoText.text = "Your new text here";
+        // infoImage.sprite = yourNewSprite; // Make sure you have a reference to yourNewSprite
+    }
+
+    public void CloseInfo()
+    {
+        infoPanel.SetActive(false); // Hide the info panel
+
+        // Lock the cursor to the center of the screen and hide it for gameplay
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        // Add logic to resume game if it was paused when the UI was active
     }
 }
