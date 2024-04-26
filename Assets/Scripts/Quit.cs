1b109b16b9ba4b5cc2b009bb732b4593cb581e6a
terencepatrck <terence2.ramirez@live.uwe.ac.uk>
2024-04-26 02:25:57 +0100

    Update Quit.cs

--- Assets/Scripts/Quit.cs
+++ Assets/Scripts/Quit.cs
@@ -13,7 +13,7 @@ public class Quit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape));
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
